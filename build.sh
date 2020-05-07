51171eec3ca754865f91f9e905e46ec3d816c0c5
Michał Mrozek <Michsior14@users.noreply.github.com>
2020-05-07 13:07:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker run --rm --privileged \
     -v ~/.docker:/root/.docker \
     homeassistant/amd64-builder \
     --all \
-    -r https://github.com/korylprince/hassio-caddy.git \
+    -r https://github.com/Michsior14/hassio-caddy.git \
     -b master \
     -t caddy
