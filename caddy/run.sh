f29ca258c3f5e4311684e5ac64c34dd2d5cf7ddb
Michał Mrozek <Michsior14@users.noreply.github.com>
2020-05-07 13:13:17 +0200

    Update run.sh

--- caddy/run.sh
+++ caddy/run.sh
@@ -3,18 +3,17 @@ set -e
 
 CONFIG_PATH=/data/options.json
 CADDY_SHARE_PATH="/share/caddy"
-CADDY_PATH=/usr/sbin/caddy
 
-DEFAULT_ARGS=( "-conf" "$CADDY_SHARE_PATH/Caddyfile" )
+DEFAULT_ARGS=( "" )
 readarray -t FLAGS < <(jq --raw-output '.flags[]' $CONFIG_PATH)
 readarray -t ENV_VARS < <(jq --raw-output '.env_vars[]' $CONFIG_PATH)
 
 if [ -f "$CADDY_SHARE_PATH/caddy.bin" ]; then
     CADDY_PATH="$CADDY_SHARE_PATH/caddy.bin"
-    echo "Found custom Caddy: $($CADDY_PATH -version)"
+    echo "Found Caddy: $($CADDY_PATH -version)"
+    echo "Running Caddy: ${ENV_VARS[*]} $CADDY_PATH ${DEFAULT_ARGS[*]} ${FLAGS[*]}"
+    exec env -S "${ENV_VARS[*]}" $CADDY_PATH ${DEFAULT_ARGS[*]} ${FLAGS[*]}
 else
-    echo "Using built-in Caddy: $($CADDY_PATH -version)"
+    echo "No Caddy binary provided"
 fi
 
-echo "Running Caddy: ${ENV_VARS[*]} $CADDY_PATH ${DEFAULT_ARGS[*]} ${FLAGS[*]}"
-exec env -S "${ENV_VARS[*]}" $CADDY_PATH ${DEFAULT_ARGS[*]} ${FLAGS[*]}
